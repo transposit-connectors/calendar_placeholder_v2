ffc914a8ce5287928936d252d04aad794a3d11d4
Nina Yang <nina@transposit.com>
2019-02-28 01:24:20 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -1,29 +1,29 @@
 (params) => {
   var moment = require('moment-timezone-with-data.js');
   var now = moment();
-  
-  var userInfo = params.userRecord;
-  api.log(userInfo.syncToken);
 
-  api.log(api.run("this.get_events_by_synctoken", {syncToken: userInfo.syncToken}));
-  var eventsCall = api.run("this.get_events_by_synctoken", {syncToken: userInfo.syncToken})[0];
+  var userRecord = params.userRecord;
+  api.log(userRecord.syncToken);
+
+  api.log(api.run("this.get_events_by_synctoken", {syncToken: userRecord.syncToken}));
+  var eventsCall = api.run("this.get_events_by_synctoken", {syncToken: userRecord.syncToken})[0];
   api.log(eventsCall);
 
   var bulkOperations = [];
   eventsCall.items.forEach((calEvent) => {
-    api.log(moment(calEvent.created).diff(moment(userInfo.lastTime), 'seconds'))
-    if (moment(calEvent.created).diff(moment(userInfo.lastTime), 'seconds') > 0) {
+    api.log(moment(calEvent.created).diff(moment(userRecord.lastTime), 'seconds'))
+    if (moment(calEvent.created).diff(moment(userRecord.lastTime), 'seconds') > 0) {
       // shortcut to tell us that this event was just created
       bulkOperations.push({operation: "this.copy_calendar_event", parameters: {personalEvent: calEvent}});
     }
   });
-  
+
   // Refresh the airtable entry
-  bulkOperations.push({operation: "this.AirtableAction", parameters: {action: "UPDATE", recordId: userInfo.recordId, "fields": {syncToken: eventsCall.nextSyncToken, lastTime: now}}});
-  
+  bulkOperations.push({operation: "this.AirtableAction", parameters: {action: "UPDATE", recordId: userRecord.recordId, "fields": {syncToken: eventsCall.nextSyncToken, lastTime: now}}});
+
   api.log(bulkOperations);
   api.runBulk(bulkOperations);
-  
+
   return "done";
 }
 
