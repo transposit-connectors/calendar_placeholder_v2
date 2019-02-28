b5231b42d39cb76ce5b4d5790b6c93983f09cd74
Nina Yang <nina@transposit.com>
2019-02-28 23:29:38 +0000

    Commit from Transposit IDE

---
+++ src/untitled_1.js
@@ -0,0 +1,8 @@
+(params) => {
+  return api.run("this.StopWatch", {webhookId: "transposit-calendar-copy-app", resourceId: "mGVoYmwoxyPmXOLg_5t3kTkvLAQ"}, {asUser: "petra@squareup.com"});
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://api-composition.transposit.com/references/js-operations
+ */
