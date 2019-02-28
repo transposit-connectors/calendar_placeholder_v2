a9a30ee0fa3353e51fe7fe75fe66de92d726c2be
Nina Yang <nina@transposit.com>
2019-02-28 02:52:12 +0000

    Commit from Transposit IDE

--- src/copy_calendar_event.js
+++ src/copy_calendar_event.js
@@ -1,5 +1,8 @@
 (params) => {
-  var newEvent = {summary: "busy", start: params.personalEvent.start, end: params.personalEvent.end}
+  var newEvent = {summary: "busy", 
+                  start: params.personalEvent.start, 
+                  end: params.personalEvent.end,
+                  reminders: {useDefault : false}
   api.run("work_google_calendar.create_calendar_event", {calendarId: 'primary', $body: newEvent});
 
   return {
