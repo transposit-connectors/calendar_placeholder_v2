9e0752b5e9f288d95a87931018dd5eea7c272430
Nina Yang <nina@transposit.com>
2019-03-01 00:25:36 +0000

    Commit from Transposit IDE

--- src/refresh_all_webhooks.js
+++ src/refresh_all_webhooks.js
@@ -1,8 +1,15 @@
 (params) => {
   try {
   	var webhookId = 'transposit-calendar-copy-app'
+    var userRecords = api.run("this.AirtableAction", {action: "GET_ALL"});
     
-  	var ran = api.runForAllUsers("this.setup_webhook", {webhookId: webhookId});
+    var users = [];
+    userRecords.forEach((record) => {
+    	users.push(record.fields.email);
+    });
+
+    // Only run for users that have airtable entries!
+  	var ran = api.runForAllUsers("this.setup_webhook", {webhookId: webhookId}, {users: users});
   } catch (err) {
     // We'll error if the webhook doesn't need to be refreshed
     api.log("Error: " + err.message);
