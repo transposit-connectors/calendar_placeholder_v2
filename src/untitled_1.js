4362ec87af04602702c0cd219fc38339ae7bc4ec
Nina Yang <nina@transposit.com>
2019-03-01 00:25:58 +0000

    Commit from Transposit IDE

--- src/untitled_1.js
+++
@@ -1,8 +0,0 @@
-(params) => {
-  api.runForAllUsers("this.get_initial_events");
-}
-
-/*
- * For sample code and reference material, visit
- * https://api-composition.transposit.com/references/js-operations
- */
